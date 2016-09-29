784f186f68492a93edc5c62d42cbe852c1a0c488
j9406 <trueno94@hotmail.com>
2016-09-29 10:24:25 -0600

    agregado comentarios al index.jade

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require("express");
 
 var app = express();
+//app.set -> este es el que permite con express usar un motor de templates en este caso JADE
 app.set("view engine", "jade");
-
+//app.get: Con este hacemos nuestro servidor
 app.get("/", function(req, res){
   var saludo = "Hola Mil y asi !!";
   res.render("index");
